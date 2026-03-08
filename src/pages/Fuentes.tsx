1bd04e7587c08b1226d479dd50b903fefa02748b
Alan Chan <alanchanzepeda@gmail.com>
2026-03-07 22:22:03 -0600

    Update Fuentes.tsx

--- src/pages/Fuentes.tsx
+++ src/pages/Fuentes.tsx
@@ -13,15 +13,27 @@ const Fuentes = () => {
         <h1 className={styles.title}>Fuentes de Consulta</h1>
 
         <div className={styles.list}>
+
           <div className={styles.ref}>
-            INEGI. (2023). <em>Estadísticas de población en México.</em> Instituto Nacional de Estadística y Geografía. Recuperado de https://www.inegi.org.mx
+            Méndez Morales, J. S. (2012). <em>Problemas económicos de México y sustentabilidad</em> (7ª ed.). 
+            McGraw-Hill / Interamericana Editores.
           </div>
+
           <div className={styles.ref}>
-            SEMARNAT. (2022). <em>Recursos naturales en México: Informe de la situación del medio ambiente.</em> Secretaría de Medio Ambiente y Recursos Naturales. Recuperado de https://www.gob.mx/semarnat
+            Instituto Nacional de Estadística y Geografía (INEGI). (2023). 
+            <em>Estadísticas de población en México.</em> Recuperado de https://www.inegi.org.mx
           </div>
+
           <div className={styles.ref}>
-            Banco Mundial. (2023). <em>Desarrollo económico en México: Indicadores y perspectivas.</em> Grupo Banco Mundial. Recuperado de https://www.bancomundial.org
+            Secretaría de Medio Ambiente y Recursos Naturales (SEMARNAT). (2022). 
+            <em>Informe de la situación del medio ambiente en México.</em> Recuperado de https://www.gob.mx/semarnat
           </div>
+
+          <div className={styles.ref}>
+            Banco Mundial. (2023). 
+            <em>Indicadores de desarrollo económico en México.</em> Recuperado de https://www.bancomundial.org
+          </div>
+
         </div>
       </div>
     </div>
