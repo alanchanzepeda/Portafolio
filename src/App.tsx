4035332a0115d70e037170f8491636d3a0c8b655
Alan Chan <alanchanzepeda@gmail.com>
2026-03-07 23:40:57 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import RecursosNaturales from './pages/RecursosNaturales';
-import SectoresEconomicos from './pages/SectoresEconomicos';
-import Autor from './pages/Autor';
-import Fuentes from './pages/Fuentes';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/recursos-naturales" element={<RecursosNaturales />} />
-      <Route path="/sectores-economicos" element={<SectoresEconomicos />} />
-      <Route path="/autor" element={<Autor />} />
-      <Route path="/fuentes" element={<Fuentes />} />
-    </Routes>
-  </BrowserRouter>
-);
+import Home from "./pages/Home";
+import RecursosNaturales from "./pages/RecursosNaturales";
+import SectoresEconomicos from "./pages/SectoresEconomicos";
+
+import Autor from "./pages/Autor";
+import Fuentes from "./pages/Fuentes";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/recursos-naturales" element={<RecursosNaturales />} />
+        <Route path="/sectores-economicos" element={<SectoresEconomicos />} />
+
+        <Route path="/autor" element={<Autor />} />
+        <Route path="/fuentes" element={<Fuentes />} />
+
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
