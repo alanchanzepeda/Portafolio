5d381e22dca8614d981f7499c5f56a1ae2317c3a
Alan Chan <alanchanzepeda@gmail.com>
2026-05-19 13:06:29 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import Home from "./pages/Home";
 import RecursosNaturales from "./pages/RecursosNaturales";
 import SectoresEconomicos from "./pages/SectoresEconomicos";
 
+import Subcompetencia3 from "./pages/Subcompetencia3";
+import Subcompetencia4 from "./pages/Subcompetencia4";
+
 import Autor from "./pages/Autor";
 import Fuentes from "./pages/Fuentes";
 import NotFound from "./pages/NotFound";
@@ -17,6 +20,9 @@ const App = () => {
 
         <Route path="/recursos-naturales" element={<RecursosNaturales />} />
         <Route path="/sectores-economicos" element={<SectoresEconomicos />} />
+
+        <Route path="/subcompetencia-3" element={<Subcompetencia3 />} />
+        <Route path="/subcompetencia-4" element={<Subcompetencia4 />} />
 
         <Route path="/autor" element={<Autor />} />
         <Route path="/fuentes" element={<Fuentes />} />
